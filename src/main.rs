0ba1e588557f134d9eff290e6df696e823a17c86
Tijmen de Jong <eengamer2007@gmail.com>
2025-04-23 11:06:04 +0200

    it can type A, and it logs to /dev/ttyACM0

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,99 @@
+
 #![no_std]
 #![no_main]
 
-#[allow(unused)]
-use log::{error, info, log, trace, warn};
-
 use core::sync::atomic::{AtomicBool, Ordering};
 
-use embassy_futures::join::{join, join3, join_array};
-
+//use defmt::*;
+use log::{info, warn};
 use embassy_executor::Spawner;
-
+use embassy_futures::join::join;
 use embassy_rp::bind_interrupts;
-use embassy_rp::block::ImageDef;
-use embassy_rp::clocks::ClockConfig;
-use embassy_rp::config::Config;
+use embassy_rp::gpio::{Input, Pull};
 use embassy_rp::peripherals::USB;
-use embassy_rp::usb::{Driver, InterruptHandler as UsbInt};
-use embassy_rp::peripherals::PIO0;
-use embassy_rp::pio::{InterruptHandler as PioInt, Pio};
-use embassy_rp::pio_programs::ws2812::*;
-use embassy_rp::gpio::Input;
-
-use embassy_usb::{
-    class::hid::{HidReaderWriter, ReportId, RequestHandler, State as HidState},
-    class::cdc_acm::CdcAcmClass,
-    control::OutResponse,
-    {Builder, Config as UsbConfig, Handler},
-};
-
-use embassy_time::Timer;
-
+use embassy_rp::usb::{Driver, InterruptHandler};
+use embassy_usb::class::cdc_acm::CdcAcmClass;
+use embassy_usb::class::hid::{HidReaderWriter, ReportId, RequestHandler, State};
+use embassy_usb::control::OutResponse;
+use embassy_usb::{Builder, Config, Handler};
 use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
-
 use {defmt_rtt as _, panic_probe as _};
 
-use smart_leds::RGB8;
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    // sparkfun pro micro rp2350 has a 12MHz crystal (just like the pico 2)
-    let p = embassy_rp::init(Config::new(ClockConfig::crystal(12_000_000)));
-    let driver = Driver::new(p.USB, IrqsUsb);
+    let p = embassy_rp::init(Default::default());
+    // Create the driver, from the HAL.
+    let driver = Driver::new(p.USB, Irqs);
 
-    let mut config = UsbConfig::new(0xc0de, 0xcafe);
-    config.manufacturer = Some("me");
-    config.product = Some("definitely a keyboard");
-    config.serial_number = Some("why is this called a serial number");
-    // max 100mA of power
+    // Create embassy-usb Config
+    let mut config = Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Me.");
+    config.product = Some("most definitely a keyboard");
+    config.serial_number = Some("12345678");
     config.max_power = 100;
-    // somehow also related to speed?
     config.max_packet_size_0 = 64;
 
-    // buffers for building the descriptors
-    let mut config_desc = [0; 256];
-    let mut bos_desc = [0; 256];
-    // microsoft OS descriptor?
-    let mut msos_desc = [0; 256];
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    // You can also add a Microsoft OS descriptor.
+    let mut msos_descriptor = [0; 256];
     let mut control_buf = [0; 64];
-    let mut recv_handler = RecvHandler {};
-    let mut device_handler = DevHandler::new();
+    let mut request_handler = MyRequestHandler {};
+    let mut device_handler = MyDeviceHandler::new();
 
-    let mut state = HidState::new();
+    // needs to be made before the builder
+    let mut state = State::new();
 
-    let mut status = embassy_usb::class::cdc_acm::State::new();
+    let mut s_state = embassy_usb::class::cdc_acm::State::new();
 
     let mut builder = Builder::new(
         driver,
         config,
-        &mut config_desc,
-        &mut bos_desc,
-        &mut msos_desc,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut msos_descriptor,
         &mut control_buf,
     );
 
     builder.handler(&mut device_handler);
 
+    // Create classes on the builder.
     let config = embassy_usb::class::hid::Config {
         report_descriptor: KeyboardReport::desc(),
         request_handler: None,
         poll_ms: 60,
         max_packet_size: 64,
     };
-
     let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
+    let serial = CdcAcmClass::new(&mut builder, &mut s_state, 64);
+    //spawner.must_spawn(logger);
 
-    //let serial = CdcAcmClass::new(&mut builder, &mut status, 64);
-    //spawner.must_spawn(logger_task(serial));
-
+    // Build the builder.
     let mut usb = builder.build();
 
+    // Run the USB device.
     let usb_fut = usb.run();
+
+    // Set up the signal pin that will be used to trigger the keyboard.
+    let mut signal_pin = Input::new(p.PIN_29, Pull::Up);
+
+    // Enable the schmitt trigger to slightly debounce.
+    signal_pin.set_schmitt(true);
 
     let (reader, mut writer) = hid.split();
 
-    //log::info!("Blinky example started");
-
+    // Do stuff with the class!
     let in_fut = async {
-
-        let mut signal_pin = Input::new(p.PIN_29, embassy_rp::gpio::Pull::None);
-        signal_pin.set_schmitt(true);
-
         loop {
-            info!("Waiting for HIGH on pin 16");
-            signal_pin.wait_for_high().await;
-            info!("HIGH DETECTED");
+            info!("Waiting for LOW on pin 29 (A3)");
+            signal_pin.wait_for_low().await;
+            info!("LOW DETECTED");
             // Create a report with the A key pressed. (no shift modifier)
             let report = KeyboardReport {
                 keycodes: [4, 0, 0, 0, 0, 0],
@@ -116,8 +106,8 @@ async fn main(_spawner: Spawner) {
                 Ok(()) => {}
                 Err(e) => warn!("Failed to send report: {:?}", e),
             };
-            signal_pin.wait_for_low().await;
-            info!("LOW DETECTED");
+            signal_pin.wait_for_high().await;
+            info!("HIGH DETECTED");
             let report = KeyboardReport {
                 keycodes: [0, 0, 0, 0, 0, 0],
                 leds: 0,
@@ -132,43 +122,22 @@ async fn main(_spawner: Spawner) {
     };
 
     let out_fut = async {
-        reader.run(false, &mut recv_handler).await;
+        reader.run(false, &mut request_handler).await;
     };
 
-    /*let led = async {
-        let Pio {
-            mut common, sm0, ..
-        } = Pio::new(p.PIO0, IrqsPio);
-
-        let mut data = [RGB8::new(0, 0, 0); 1];
-
-        let program = PioWs2812Program::new(&mut common);
-        let mut led = PioWs2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_25, &program);
-
-        loop {
-            log::info!("LED ON");
-            data = data.map(|_| RGB8::new(0x05, 0x05, 0x05));
-            led.write(&data).await;
-            Timer::after_millis(1000).await;
-
-            log::info!("LED OFF");
-            data = data.map(|_| RGB8::new(0x00, 0x00, 0x00));
-            led.write(&data).await;
-            Timer::after_millis(1000).await;
-        }
-    };*/
-
-    /*let logger = async {
-        embassy_usb_logger::with_class!(1024, log::LevelFilter::Info, serial).await;
-    };*/
-
-    //join_array([usb_fut, /*logger,*/ in_fut, out_fut, /*led*/]).await;
-    join3(usb_fut, in_fut, out_fut).await;
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, join(join(in_fut, out_fut), embassy_usb_logger::with_class!(1024, log::LevelFilter::Trace, serial))).await;
 }
 
-struct RecvHandler {}
+//#[embassy_executor::task]
+//async fn logger(class: CdcAcmClass<>) {
+//    embassy_usb_logger::with_class!(1024, log::LevelFilter::Trace, class).await;
+//}
 
-impl RequestHandler for RecvHandler {
+struct MyRequestHandler {}
+
+impl RequestHandler for MyRequestHandler {
     fn get_report(&mut self, id: ReportId, _buf: &mut [u8]) -> Option<usize> {
         info!("Get report for {:?}", id);
         None
@@ -189,26 +158,25 @@ impl RequestHandler for RecvHandler {
     }
 }
 
-struct DevHandler {
+struct MyDeviceHandler {
     configured: AtomicBool,
 }
 
-impl DevHandler {
+impl MyDeviceHandler {
     fn new() -> Self {
-        DevHandler {
+        MyDeviceHandler {
             configured: AtomicBool::new(false),
         }
     }
 }
 
-impl Handler for DevHandler {
+impl Handler for MyDeviceHandler {
     fn enabled(&mut self, enabled: bool) {
-        self.configured
-            .store(false, core::sync::atomic::Ordering::Relaxed);
+        self.configured.store(false, Ordering::Relaxed);
         if enabled {
-            info!("enabled");
+            info!("Device enabled");
         } else {
-            info!("disabled");
+            info!("Device disabled");
         }
     }
 
@@ -225,42 +193,10 @@ impl Handler for DevHandler {
     fn configured(&mut self, configured: bool) {
         self.configured.store(configured, Ordering::Relaxed);
         if configured {
-            info!(
-                "Device configured, it may now draw up to the configured current limit from Vbus."
-            )
+            info!("Device configured, it may now draw up to the configured current limit from Vbus.")
         } else {
             info!("Device is no longer configured, the Vbus current limit is 100mA.");
         }
     }
 }
-/*
-#[embassy_executor::task]
-async fn logger_task(class: CdcAcmClass<'static, dyn UsbDriver<'static, USB>>) {
-    embassy_usb_logger::with_class!(1024, log::LevelFilter::Info, class);
-}
-*/
 
-bind_interrupts!(struct IrqsUsb {
-    USBCTRL_IRQ => UsbInt<USB>;
-});
-
-bind_interrupts!(struct IrqsPio {
-    PIO0_IRQ_0 => PioInt<PIO0>;
-});
-
-#[unsafe(link_section = ".start_block")]
-#[used]
-pub static IMAGE_DEF: ImageDef = ImageDef::secure_exe();
-
-// Program metadata for `picotool info`.
-// This isn't needed, but it's recomended to have these minimal entries.
-#[unsafe(link_section = ".bi_entries")]
-#[used]
-pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
-    embassy_rp::binary_info::rp_program_name!(c"Rust Mechanical Keyboard (rsmk)"),
-    embassy_rp::binary_info::rp_program_description!(
-        c"Firmware for (split) mechanical keyboards written in rust"
-    ),
-    embassy_rp::binary_info::rp_cargo_version!(),
-    embassy_rp::binary_info::rp_program_build_attribute!(),
-];
